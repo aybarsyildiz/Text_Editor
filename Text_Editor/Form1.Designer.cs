ddcea6ce2c5c6edd6e2fdee6b9c96694d4c89207
Aybars <42347579+aybarsyildiz@users.noreply.github.com>
2019-03-26 19:48:42 +0300

    Add files via upload

---
+++ Text_Editor/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Text_Editor
+{
+    partial class TextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.boldButton = new System.Windows.Forms.Button();
+            this.buttonItalic = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.blackButton = new System.Windows.Forms.Button();
+            this.greyButton = new System.Windows.Forms.Button();
+            this.yellowButton = new System.Windows.Forms.Button();
+            this.redButton = new System.Windows.Forms.Button();
+            this.greenButton = new System.Windows.Forms.Button();
+            this.blueButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nightMode = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.underLine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 115);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1003, 400);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // boldButton
+            // 
+            this.boldButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.boldButton.Location = new System.Drawing.Point(227, 43);
+            this.boldButton.Name = "boldButton";
+            this.boldButton.Size = new System.Drawing.Size(28, 24);
+            this.boldButton.TabIndex = 1;
+            this.boldButton.Text = "b";
+            this.boldButton.UseVisualStyleBackColor = true;
+            this.boldButton.Click += new System.EventHandler(this.boldButton_Click);
+            // 
+            // buttonItalic
+            // 
+            this.buttonItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonItalic.Location = new System.Drawing.Point(261, 43);
+            this.buttonItalic.Name = "buttonItalic";
+            this.buttonItalic.Size = new System.Drawing.Size(27, 24);
+            this.buttonItalic.TabIndex = 2;
+            this.buttonItalic.Text = "I";
+            this.buttonItalic.UseVisualStyleBackColor = true;
+            this.buttonItalic.Click += new System.EventHandler(this.buttonItalic_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(345, 43);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(74, 24);
+            this.hScrollBar1.TabIndex = 5;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(359, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Text Size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "   ";
+            // 
+            // blackButton
+            // 
+            this.blackButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.blackButton.Location = new System.Drawing.Point(461, 44);
+            this.blackButton.Name = "blackButton";
+            this.blackButton.Size = new System.Drawing.Size(24, 23);
+            this.blackButton.TabIndex = 8;
+            this.blackButton.Text = "button1";
+            this.blackButton.UseVisualStyleBackColor = false;
+            this.blackButton.Click += new System.EventHandler(this.blackButton_Click);
+            // 
+            // greyButton
+            // 
+            this.greyButton.BackColor = System.Drawing.Color.Gray;
+            this.greyButton.Location = new System.Drawing.Point(491, 44);
+            this.greyButton.Name = "greyButton";
+            this.greyButton.Size = new System.Drawing.Size(24, 23);
+            this.greyButton.TabIndex = 9;
+            this.greyButton.UseVisualStyleBackColor = false;
+            this.greyButton.Click += new System.EventHandler(this.greyButton_Click);
+            // 
+            // yellowButton
+            // 
+            this.yellowButton.BackColor = System.Drawing.Color.Gold;
+            this.yellowButton.ForeColor = System.Drawing.Color.Yellow;
+            this.yellowButton.Location = new System.Drawing.Point(521, 44);
+            this.yellowButton.Name = "yellowButton";
+            this.yellowButton.Size = new System.Drawing.Size(24, 23);
+            this.yellowButton.TabIndex = 10;
+            this.yellowButton.UseVisualStyleBackColor = false;
+            this.yellowButton.Click += new System.EventHandler(this.yellowButton_Click);
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.Red;
+            this.redButton.Location = new System.Drawing.Point(551, 44);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(24, 23);
+            this.redButton.TabIndex = 11;
+            this.redButton.UseVisualStyleBackColor = false;
+            this.redButton.Click += new System.EventHandler(this.redButton_Click);
+            // 
+            // greenButton
+            // 
+            this.greenButton.BackColor = System.Drawing.Color.Green;
+            this.greenButton.Location = new System.Drawing.Point(581, 44);
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(24, 23);
+            this.greenButton.TabIndex = 12;
+            this.greenButton.UseVisualStyleBackColor = false;
+            this.greenButton.Click += new System.EventHandler(this.greenButton_Click);
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.Blue;
+            this.blueButton.Location = new System.Drawing.Point(611, 44);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(24, 23);
+            this.blueButton.TabIndex = 13;
+            this.blueButton.UseVisualStyleBackColor = false;
+            this.blueButton.Click += new System.EventHandler(this.blueButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(536, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Text Color";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(641, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 23);
+            this.button1.TabIndex = 15;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nightMode
+            // 
+            this.nightMode.Location = new System.Drawing.Point(741, 35);
+            this.nightMode.Name = "nightMode";
+            this.nightMode.Size = new System.Drawing.Size(53, 40);
+            this.nightMode.TabIndex = 16;
+            this.nightMode.Text = "OFF";
+            this.nightMode.UseVisualStyleBackColor = true;
+            this.nightMode.Click += new System.EventHandler(this.nightMode_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(738, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Night Mode";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yellowtail", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(7, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 26);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Text Editor 0.1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ink Free", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(943, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 14);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "aybarsyildiz";
+            // 
+            // underLine
+            // 
+            this.underLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.underLine.Location = new System.Drawing.Point(294, 43);
+            this.underLine.Name = "underLine";
+            this.underLine.Size = new System.Drawing.Size(27, 24);
+            this.underLine.TabIndex = 20;
+            this.underLine.Text = "u";
+            this.underLine.UseVisualStyleBackColor = true;
+            this.underLine.Click += new System.EventHandler(this.underLine_Click);
+            // 
+            // TextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(1027, 527);
+            this.Controls.Add(this.underLine);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nightMode);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.greenButton);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.yellowButton);
+            this.Controls.Add(this.greyButton);
+            this.Controls.Add(this.blackButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.buttonItalic);
+            this.Controls.Add(this.boldButton);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "TextEditor";
+            this.Text = "Text Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button boldButton;
+        private System.Windows.Forms.Button buttonItalic;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button blackButton;
+        private System.Windows.Forms.Button greyButton;
+        private System.Windows.Forms.Button yellowButton;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Button greenButton;
+        private System.Windows.Forms.Button blueButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button nightMode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button underLine;
+    }
+}
+
---
+++ Text_Editor/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Editor
+{
+    public partial class TextEditor : Form
+    {
+        public TextEditor()
+        {
+            InitializeComponent();
+        }
+        int boldCheck = 0;
+        private void boldButton_Click(object sender, EventArgs e)
+        {
+            if (boldCheck == 0)
+            {
+                textBox1.Font = new Font(textBox1.Font, FontStyle.Bold);
+                boldCheck = 1;
+            }
+            else { 
+
+                textBox1.Font = new Font(textBox1.Font, FontStyle.Regular);
+                boldCheck = 0;
+            }
+        }
+        int italicCheck = 0;
+        private void buttonItalic_Click(object sender, EventArgs e)
+        {
+            if (italicCheck == 0)
+            {
+                textBox1.Font = new Font(textBox1.Font, FontStyle.Italic);
+                italicCheck = 1;
+            }
+            else
+            {
+
+                textBox1.Font = new Font(textBox1.Font, FontStyle.Regular);
+                italicCheck = 0;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Tab)
+            {
+                textBox1.Text += "    ";
+            }
+            
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (hScrollBar1.Value == 0)
+                hScrollBar1.Value = 5;
+
+            textBox1.Font = new Font("Serif", hScrollBar1.Value,FontStyle.Regular);
+            label2.Text = Convert.ToString(hScrollBar1.Value);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void blackButton_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Black;
+ 
+        }
+
+        private void greyButton_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Gray;
+        }
+
+        private void yellowButton_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Gold;
+        }
+
+        private void redButton_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void greenButton_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Green;
+        }
+
+        private void blueButton_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Blue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+        }
+
+        private void nightMode_Click(object sender, EventArgs e)
+        {
+            
+            if (nightMode.Text == "OFF") {
+                if (textBox1.ForeColor == System.Drawing.Color.Black)
+                    textBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+
+
+                BackColor = System.Drawing.Color.Gray;
+         
+                textBox1.BackColor = System.Drawing.Color.Black;
+                nightMode.Text = "ON";
+            }
+            else
+            {
+                if (textBox1.ForeColor == System.Drawing.Color.WhiteSmoke)
+                    textBox1.ForeColor = System.Drawing.Color.Black;
+                BackColor = System.Drawing.Color.PowderBlue;
+                textBox1.BackColor = System.Drawing.Color.White;
+                nightMode.Text = "OFF";
+
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.ScrollBars = ScrollBars.Both;
+        }
+
+        private void underLine_Click(object sender, EventArgs e)
+        {
+            textBox1.Font = new Font(textBox1.Font, FontStyle.Underline);
+        }
+    }
+}
